2504abf2eb1581e4985213d2f4a59753d16c4ab1
Bikram Gurung <grgbiki.git@gmail.com>
2025-01-27 22:33:56 +0545

    v2

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('academics/', views.academics, name='academics'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def academics(request):
+    return render(request, 'core/academics.html')
